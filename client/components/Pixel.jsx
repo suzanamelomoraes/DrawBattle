07d54e0a5078adfa5edc06c4d4946e3861ed7bb7
Ashleigh Dominique Ah Ta <ashleigh.ahta@gmail.com>
2019-11-08 11:26:32 +1300

    create mouse event functions for draw and erase

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 
+// const HexColor = () =>
+//   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+// on-click until user finds color
+// once color is found, user can drag cursor?
+
 class Pixel extends React.Component {
   state = {
     backgroundColor: 'blanchedalmond'
+  }
+  draw = () => {
+    this.setState(
+      { backgroundColor: 'lime' }
+    )
+  }
+  erasePixel = (event) => {
+    event.preventDefault()
+    this.setState(
+      // on right-click, erase colored pixel
+      // prevent default setting
+
+      { backgroundColor: 'blanchedalmond' }
+    )
   }
 
   render () {
     const stylie = { height: '30px', width: '30px', backgroundColor: this.state.backgroundColor }
     return (
     // <>
-      
-      <div className='pixel' style={stylie}></div>
+      <div className='pixel' style={stylie} onClick={this.draw} onContextMenu={this.erasePixel}></div>
+
     // </>
     )
   }
