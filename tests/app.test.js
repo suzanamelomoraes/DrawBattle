fc66049b76b241df4e60b07adfcfd38f536db357
Andria Cristia Hibe <andriacohibe@gmail.com>
2019-11-08 15:11:30 +1300

    Add tests for App component

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import { mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import App from '../client/components/App'
+import Pixel from '../client/components/Pixel'
 
 test('<App />', () => {
-  const expected = 'React development has begun!'
+  const expected = 'Draw Battle!'
   const wrapper = mount(<App />)
   expect(wrapper.text()).toMatch(expected)
+})
+
+test('contains <Pixel /> (shallow)', () => {
+  const expected = '<Pixel />'
+
+  const wrapper = shallow(<App />)
+  const actual = wrapper.text()
+
+  expect(actual).toMatch(expected)
+})
+
+test('contains <Pixel /> (mount)', () => {
+  const expected = true
+
+  const wrapper = mount(<App />)
+  const actual = wrapper.containsMatchingElement(Pixel)
+
+  expect(actual).toBe(expected)
+})
+
+test('calls mockFunction when Start button is pressed', () => {
+  const mockFn = jest.fn()
+
+  const testClick = shallow(
+    <button onClick={mockFn}></button>
+  )
+
+  testClick.simulate('click')
+  expect(mockFn).toHaveBeenCalled()
 })
